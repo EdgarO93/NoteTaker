790445e79f2b4c1a9dc3517eb2e82044b4d05e00
Edgar Ortega <edort93@gmail.com>
2021-10-09 20:12:50 -0500

    updated comment

--- server.js
+++ server.js
@@ -15,7 +15,7 @@ app.use(express.json());
 app.use(express.urlencoded({ extended: true }));
 app.use(express.static('public'));
 
-// GET Route for homepage
+// GET Route for notes 
 app.get('/', (req, res) =>
   res.sendFile(path.join(__dirname, '/notes.html'))
 );
