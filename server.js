c6504546ac2913729c09c3aff33d1c54722084d6
Edgar Ortega <edort93@gmail.com>
2021-10-09 20:03:18 -0500

    added middleware for parsing JSON

--- server.js
+++ server.js
@@ -10,6 +10,11 @@ const PORT = 3001;
 
 const app = express();
 
+// Middleware for parsing JSON and urlencoded form data
+app.use(express.json());
+app.use(express.urlencoded({ extended: true }));
+app.use(express.static('public'));
+
 // GET Route for homepage
 app.get('/', (req, res) =>
   res.sendFile(path.join(__dirname, '/notes.html'))
