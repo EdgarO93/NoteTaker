06baf17308c41f5c4f5f2341da7e794c8ee1c3db
Edgar Ortega <edort93@gmail.com>
2021-10-11 22:30:36 -0500

    updated comments

--- server.js
+++ server.js
@@ -1,13 +1,12 @@
 const express = require('express');
 const path = require('path');
 const fs = require('fs');
-// const util = require('util');
-// const noteData = require('./db/db.json')
+
 
 // Helper method for generating unique ids
 const uuid = require('./public/assets/js/helpers/uuid');
 
-const PORT = process.env.port || 3001;
+const PORT = process.env.PORT || 3001;
 
 const app = express();
 
